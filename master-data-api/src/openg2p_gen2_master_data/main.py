815addd771c33c9821b793076a77e77a17a80e8f
Puneet  Joshi <puneet@openg2p.org>
2026-07-13 18:00:04 +0530

    G2P-5341 Fix master-data-api boot crash: source keycloak_client_id from iam-core auth config

--- master-data-api/src/openg2p_gen2_master_data/main.py
+++ master-data-api/src/openg2p_gen2_master_data/main.py
@@ -9,6 +9,7 @@ from openg2p_gen2_master_data.app import Initializer
 from openg2p_fastapi_common.ping import PingInitializer
 
 from iam_core.user_auth.app import Initializer as IAMInitializer
+from iam_core.user_auth.config import Settings as IAMAuthSettings
 from iam_core.user_auth.middleware import (
     ResolvePermissionMiddleware,
     ValidateAndRefreshTokenMiddleware,
@@ -18,7 +19,9 @@ IAMInitializer()
 initializer = Initializer()
 PingInitializer()
 
-_config = Settings.get_config()
+# Auth config (keycloak_client_id, auth_provider_api_url) is owned by iam-core
+# and read from its own COMMON_* env, not from master-data's Settings.
+_iam_auth_config = IAMAuthSettings.get_config()
 
 app = initializer.return_app()
 
@@ -26,7 +29,7 @@ app = initializer.return_app()
 # ValidateAndRefreshToken -> ResolvePermission -> app
 app.add_middleware(
     ResolvePermissionMiddleware,
-    client_id=_config.keycloak_client_id,
+    client_id=_iam_auth_config.keycloak_client_id,
     allow_by_default=True,
 )
 app.add_middleware(ValidateAndRefreshTokenMiddleware)
